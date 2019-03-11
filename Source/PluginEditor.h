6a48b03c70ece62a0bc22fb9bfaea76cb77f5a66
Stephen Delaney <sdelan200@caledonian.ac.uk>
2019-03-11 13:31:12 +0000

    Code To Link Processor and Editor
    Linking of Processor and Editor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,35 +1,66 @@
 /*
   ==============================================================================
 
-    This file was auto-generated!
+  This is an automatically generated GUI class created by the Projucer!
 
-    It contains the basic framework code for a JUCE plugin editor.
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.1
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
 
   ==============================================================================
 */
 
 #pragma once
 
+//[Headers]     -- You can add your own extra header files here --
 #include "../JuceLibraryCode/JuceHeader.h"
-#include "PluginProcessor.h"
+#include "PluginProcessor.h" //reference the processor from the editor class.
+//[/Headers]
+
+
 
 //==============================================================================
 /**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
 */
 class MidSideUtilityAudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
-    MidSideUtilityAudioProcessorEditor (MidSideUtilityAudioProcessor&);
+    //==============================================================================
+    MidSideUtilityAudioProcessorEditor (MidSideUtilityAudioProcessor& p);
     ~MidSideUtilityAudioProcessorEditor();
 
     //==============================================================================
-    void paint (Graphics&) override;
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
     void resized() override;
 
-private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
-    MidSideUtilityAudioProcessor& processor;
 
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+	MidSideUtilityAudioProcessor& processor; //Allows access the processor class members.
+	//[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MidSideUtilityAudioProcessorEditor)
 };
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
